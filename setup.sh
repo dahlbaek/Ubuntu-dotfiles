aeaaf08c88ef7680430eb26f7444b536b38f8576
Jonas Dahlbæk <30782351+dahlbaek@users.noreply.github.com>
2017-09-10 14:17:41 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Install standard programs
-apt-get update
-sudo -k apt-get install chromium i3 neovim python3-pip r-base ranger texlive-full zathura
+sudo apt-get update
+sudo apt-get install chromium-browser curl i3 neovim python3-pip r-base ranger texlive-full zathura
+sudo -k
 
 # Install vim-plug for vim and nvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install nvr to enable reverse-search with vim-tex in nvim
+pip3 install --upgrade pip
 pip3 install neovim-remote
 
 # Recursively create symlinks to dotfiles
