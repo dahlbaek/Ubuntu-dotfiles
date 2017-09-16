66fa590f126f335ee07be3355df43d8a11e27be6
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2017-09-16 21:38:25 +0200

    Adhere to best practices

--- dotfiledir/bin/runmutt.py
+++ dotfiledir/bin/runmutt.py
@@ -5,30 +5,32 @@ import threading
 import time
 import os
 
-AAUlog = open(os.path.expanduser('~/.config/offlineimap/AAU.log'),'w')
-AUlog = open(os.path.expanduser('~/.config/offlineimap/AU.log'),'w')
-
+# Sync accounts asynchronously, but wait for all syncs to finish
 def offlineimap():
     AAU = subprocess.Popen(['offlineimap', '-a AAU'], stderr = AAUlog)
     AU = subprocess.Popen(['offlineimap', '-a AU'], stderr = AUlog)
     AAU.communicate()
     AU.communicate()
 
+# Sync every wait_time seconds, and when mutt closes
 def autosync():
-    while muttisopen:
+    while not mutt_has_closed:
         offlineimap()
-        for i in range(1,120):
-            if muttisopen:
+        for i in range(wait_time):
+            if not mutt_has_closed:
                 time.sleep(1)
             else:
                 offlineimap()
                 break
 
-goimap = threading.Thread(target=autosync)
+wait_time = 300 # Seconds to wait between syncs
+mutt_has_closed = False
+imap_thread = threading.Thread(target=autosync)
 
-muttisopen = True
-goimap.start()
-subprocess.call('mutt')
-muttisopen = False
-
-print('Finishing mailbox sync. This may take a while.')
+# Open log files, start autosync, start mutt. When Mutt closes, wait for autosync to finish.
+with open(os.path.expanduser('~/.config/offlineimap/AAU.log'),'w') as AAUlog, open(os.path.expanduser('~/.config/offlineimap/AU.log'),'w') as AUlog:
+    imap_thread.start()
+    subprocess.call('mutt')
+    mutt_has_closed = True
+    print('Synchronizing mailbox. This may take a while.')
+    imap_thread.join()
