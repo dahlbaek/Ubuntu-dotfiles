9ae20061a5795affa0177c8cd1ab6ebbc57da126
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2018-01-01 14:04:25 +0100

    Hide output and use GNU style options

--- setup.sh
+++ setup.sh
@@ -4,24 +4,23 @@ set -euf
 IFS=''
 
 printf "\nTHIS SCRIPT DOES NOT REQUIRE ROOT PRIVILEGES. ABORT IF IT ASKS FOR THEM.\n\n"
-sleep 3
 
 # set path variables
 SCRIPTDIR="${HOME}/git/dotfiles"
 DOTFILEDIR="${SCRIPTDIR}/home"
 
 # Set simple firewall rules
-sudo /bin/sh /usr/bin/iptables.sh
+sudo /bin/sh /usr/local/bin/iptables.sh
 
 # Install vim-plug for nvim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl --create-dirs --fail --output ~/.local/share/nvim/site/autoload/plug.vim --silent --show-error -- https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install nvr to enable reverse-search with vim-tex in nvim
-pip3 install --upgrade pip
-pip3 install --user neovim neovim-remote
+pip3 install --quiet --upgrade -- pip
+pip3 install --quiet --user -- neovim neovim-remote
 
 # Recursively create symlinks to dotfiles
-cp -asfv "${DOTFILEDIR}/." "${HOME}" >setup.log
+cp --force --no-dereference --preserve=all --recursive --symbolic-link --verbose -- "${DOTFILEDIR}/." "${HOME}" >setup.log
 
 # Install plugins for nvim
 nvim -c "PlugUpdate" -c "qa"
