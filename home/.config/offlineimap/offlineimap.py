53815d1a088cb919bc654197299251569962c684
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2018-01-04 22:46:33 +0100

    Fix function

--- home/.config/offlineimap/offlineimap.py
+++ home/.config/offlineimap/offlineimap.py
@@ -1,6 +1,11 @@
-import subprocess
+"""For offlineimap."""
+
+from subprocess import check_output
+from os.path import expanduser
 
 def remote(accountinfo):
-    path = "/home/dahlbaek/.config/sensitive/mail/%s.gpg" % accountinfo
-    return subprocess.check_output(["gpg2", "--quiet", "-d", path]).strip()
+    """Pass."""
+    if accountinfo in ["AUhost", "AUuser", "AUpass", "AAUhost", "AAUuser", "AAUpass"]:
+        path = expanduser("~/.config/sensitive/mail/%s.gpg" % accountinfo)
+        return check_output(["gpg2", "--quiet", "-d", path]).strip()
 
