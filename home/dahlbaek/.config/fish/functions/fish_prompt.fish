efc4eaafdc4f4f79e53a3786218d88b26fefea38
dahlbaek <30782351+dahlbaek@users.noreply.github.com>
2018-11-28 13:54:02 +0100

    Improve fish_prompt

--- home/dahlbaek/.config/fish/functions/fish_prompt.fish
+++ home/dahlbaek/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
-    echo -n -s $USER ' ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) "> "
+    echo -n -s $USER ' '
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+    echo -n "> "
 end
